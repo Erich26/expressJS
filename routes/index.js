4e8067bf5a5798263bd9e6ae074e74a88ce48950
Erich26 <erich.fredersdorff@codeimmersives.com>
2022-05-17 12:30:45 -0400

    standard goals met

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Express Project' });
 });
 
+router.get('/datetime', (req, res) => {
+  res.render(Date())
+})
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/datetime', (req, res) => {
+  res.send(Date())
+});
+
+router.get('/myname', (req, res) => {
+  res.send('Erich')
+});
+
+router.get('/myfavoritemovies', (req, res) => {
+  const favoriteMovies = ["Lord of the Rings", 
+                         "Harry Potter", 
+                         "Fun with Dick and Jane"
+]
+  res.json(favoriteMovies);
+})
+
 module.exports = router;
